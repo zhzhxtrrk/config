05a5b01a7f8cb38a63aac5df1f7dfbb91c442086
Steven Zhang <Steven.Zhang@ActiveNetwork.com>
2012-10-09 17:36:44 +0800

    remove unused plugins

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,6 +5,29 @@
 (setq user-full-name "Steven Zhang")
 (setq user-mail-address "zhzhxtrrk@gmail.com")
 
+;; lisp load path
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+
+;; look and feel
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+	(color-theme-initialize)
+	(cond (window-system (color-theme-cyberpunk)))))
+
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode t)
+
+(cond (window-system (progn
+		       (scroll-bar-mode -1)
+		       (tool-bar-mode -1)
+		       (setq default-frame-alist '((width . 100) (height . 45)))
+		       (set-default-font "Consolas-15")
+		       (add-to-list 'default-frame-alist '(font . "Consolas-15")))))
+
+;; smooth scrolling
+(require 'smooth-scrolling)
+
 ;; common key bindings
 (global-set-key (kbd "C-m") 'newline-and-indent)
 (global-set-key [(f2)] 'shell-command)
@@ -31,36 +54,11 @@
     map)
   "keymap used in ruby mode")
 
-;; lisp load path
-(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
-
 ;; php-mode
 (require 'php-mode)
 (add-hook 'php-mode-hook
 		  '(lambda ()
 			 (flymake-mode)))
-
-;; lua-mode
-(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
-
-;; cmake-mode
-(autoload 'cmake-mode "cmake-mode" "CMake editing mode." t)
-(setq auto-mode-alist
-      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
-                ("\\.cmake\\'" . cmake-mode))
-              auto-mode-alist))
-
-;; haskell-mode
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/haskell-mode-2.8.0"))
-(require 'haskell-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
-
-;; go mode
-(require 'go-mode-load)
 
 ;; js2-mode, may need to install from elpa first, not managed in git
 (add-hook 'js2-mode-hook
@@ -71,7 +69,6 @@
 (require 'textmate)
 (textmate-mode t)
 
-
 ;; dirtree
 (require 'dirtree)
 (setq dirtree-windata '(frame left 0.2 delete))
@@ -81,43 +78,20 @@
 	   (cond (project-root (dirtree-in-buffer project-root t))
 			 (t (message "no project found")))))
 
-
 ;; git
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/magit-1.1.1"))
 (require 'magit)
 
 ;; shell
-(setq eshell-login-script "profile")
 (add-hook 'term-exec-hook
 		  #'(lambda ()
 			  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)))
 
 ;; common lisp
-(setq inferior-lisp-program "/usr/local/bin/sbcl") ; your Lisp system
+(setq inferior-lisp-program "/usr/local/bin/ccl64") ; your Lisp system
 (add-to-list 'load-path "~/.emacs.d/slime-2012-09-03")  ; your SLIME directory
 (require 'slime)
 (slime-setup)
-
-;; smooth scrolling
-(require 'smooth-scrolling)
-
-;; look and feel
-(require 'color-theme)
-(eval-after-load "color-theme"
-  '(progn
-	(color-theme-initialize)
-	(cond (window-system (color-theme-cyberpunk)))))
-
-(require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode)
-
-(cond (window-system (progn
-		       (scroll-bar-mode -1)
-		       (tool-bar-mode -1)
-		       (setq default-frame-alist '((width . 100) (height . 45)))
-		       (set-default-font "Consolas-15")
-		       (add-to-list 'default-frame-alist '(font . "Consolas-15")))))
-
 
 ;; geben debuging support
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/geben"))
