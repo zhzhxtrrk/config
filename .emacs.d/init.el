ce1def7e8b5d2a8b01df65b424bc7231876162bf
Steven Zhang <Steven.Zhang@ActiveNetwork.com>
2013-06-06 22:17:52 +0800

    replace outdated textmate.el with projectile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -53,6 +53,8 @@
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "M-`") 'my-quick-switch)
+(global-set-key (kbd "s-T") 'my-goto-symbol)
+(global-set-key (kbd "M-T") 'my-goto-symbol)
 
 ;; no startup screen
 (setq inhibit-startup-message t)
@@ -101,14 +103,74 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
-;; textmate
-(require 'textmate)
-(textmate-mode t)
+;; ido
+(ido-mode t)
+
+;; projectile
+(add-to-list 'load-path "~/.emacs.d/s.el")
+(add-to-list 'load-path "~/.emacs.d/dash.el")
+(add-to-list 'load-path "~/.emacs.d/projectile")
+(require 'projectile)
+(projectile-global-mode)
+(setq projectile-use-native-indexing t)
+(setq projectile-enable-caching t)
 
 ;; rvm.el
 (add-to-list 'load-path "~/.emacs.d/rvm.el/")
 (require 'rvm)
 (rvm-use-default)
+
+;; goto-symbol
+(require 'imenu)
+(defun my-goto-symbol ()
+  "Update the imenu index and then use ido to select a symbol to navigate to.
+Symbols matching the text at point are put first in the completion list."
+  (interactive)
+  (imenu--make-index-alist)
+  (let ((name-and-pos '())
+        (symbol-names '()))
+    (flet ((addsymbols (symbol-list)
+                       (when (listp symbol-list)
+                         (dolist (symbol symbol-list)
+                           (let ((name nil) (position nil))
+                             (cond
+                              ((and (listp symbol) (imenu--subalist-p symbol))
+                               (addsymbols symbol))
+
+                              ((listp symbol)
+                               (setq name (car symbol))
+                               (setq position (cdr symbol)))
+
+                              ((stringp symbol)
+                               (setq name symbol)
+                               (setq position
+                                     (get-text-property 1 'org-imenu-marker
+                                                        symbol))))
+
+                             (unless (or (null position) (null name))
+                               (add-to-list 'symbol-names name)
+                               (add-to-list 'name-and-pos (cons name position))))))))
+      (addsymbols imenu--index-alist))
+    ;; If there are matching symbols at point, put them at the beginning
+    ;; of `symbol-names'.
+    (let ((symbol-at-point (thing-at-point 'symbol)))
+      (when symbol-at-point
+        (let* ((regexp (concat (regexp-quote symbol-at-point) "$"))
+               (matching-symbols (delq nil
+                                       (mapcar
+                                        (lambda (symbol)
+                                          (if (string-match regexp symbol)
+                                              symbol))
+                                        symbol-names))))
+          (when matching-symbols
+            (sort matching-symbols (lambda (a b) (> (length a) (length b))))
+            (mapc (lambda (symbol)
+                    (setq symbol-names (cons symbol
+                                             (delete symbol symbol-names))))
+                  matching-symbols)))))
+    (let* ((selected-symbol (ido-completing-read "Symbol? " (reverse symbol-names)))
+           (position (cdr (assoc selected-symbol name-and-pos))))
+      (goto-char (if (overlayp position) (overlay-start position) position)))))
 
 ;; quick switch
 (defvar *my-switch-table*
